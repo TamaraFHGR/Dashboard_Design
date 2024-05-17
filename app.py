2aabbeb212919572aebd692ed99d60d6fcac4076
shr1996 <103521914+shr1996@users.noreply.github.com>
2024-05-17 12:15:29 +0200

    update

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ Definition of Dash Layout
 app.layout = html.Div([
     dcc.Tabs([
         dcc.Tab(label='Tab 1', children=[
+<<<<<<< Updated upstream
             html.Div([  # Zeile für Spalten 1 und 2
                 html.Div([  # Spalte 1
                     html.Div(
@@ -102,6 +103,68 @@ app.layout = html.Div([
                     )
                 ], className="columns_1_2")  # Ende Spalte 1
             ], className="row")  # Zeilencontainer für Spalten 1 und 2
+=======
+            html.Div([                  # Spalte 1
+                html.Div(
+                    id='container_1',            # Spalte 1 / Container 1
+                    children=[
+                        html.H1("Allgemeine Lebensqualität in der Schweiz"),
+                        html.H2("Einschätzung der subjektiven Lebensqualität"),
+                        dcc.Dropdown(
+                            id='dropdown_1',
+                            options=[{'label': color, 'value': color} for color in ['red', 'green', 'orange']],
+                            value='red',
+                            multi=False)]
+                ),
+                html.Div(
+                    id='container_2',           # Spalte 1 / Container 2
+                    children=[
+                        dcc.Graph(id='graph_1'),
+                        dcc.Slider(
+                            id='slider_1',
+                            min=df['Jahr'].min(),
+                            max=df['Jahr'].max())]
+                )
+            ], className="columns_1_2"),  # Ende Spalte 1
+
+            html.Div([  # Spalte 2
+                html.Div(
+                    id='container_3',  # Spalte 2 / Container 1
+                    children=[
+                        dcc.Graph(id='graph_2'),
+                        dcc.Dropdown(
+                            id='dropdown_3',
+                            options=[{'label': col, 'value': col} for col in df.columns[3:]],  # Nur Spalten ab Index 3
+                            value=None,
+                            multi=False
+                        )
+                    ]
+                )
+            ], className="columns_1_2")  # Ende Spalte 2
+        ]),
+        dcc.Tab(label='Tab 2', children=[
+            html.Div([  # Spalte 1
+                html.Div(
+                    id='container_4',  # Spalte 1 / Container 1
+                    children=[
+                        html.H1("Detailerete Analyse der Lebensqualität"),
+                        html.H2("Vergleich der Lebensqualität nach Geschlecht"),
+                        dcc.Dropdown(
+                            id='dropdown_4',
+                            options=[{'label': col, 'value': col} for col in df.columns],
+                            value='GDP',
+                            multi=False
+                        )
+                    ]
+                ),
+                html.Div(
+                    id='container_5',  # Spalte 1 / Container 2
+                    children=[
+                        dcc.Graph(id='graph_3')
+                    ]
+                )
+            ], className="columns_1_2")
+>>>>>>> Stashed changes
         ])
     ])
 ], className="row")  # Hauptcontainer
@@ -136,7 +199,7 @@ Define Graph 2
 
 @app.callback(
     Output('graph_2', 'figure'),
-    Input('dropdown_2', 'value'),
+    Input('dropdown_1', 'value'),
     Input('dropdown_3', 'value')
 )
 
